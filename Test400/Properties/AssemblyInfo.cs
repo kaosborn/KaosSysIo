d9079a72760de0ed5365a87db3490e08d1d88213
kaosborn <kaosborn@users.noreply.github.com>
2017-05-04 01:24:20 -0700

    add initial tests

---
+++ Test400/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Test400")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("Test400")]
+[assembly: AssemblyCopyright ("Copyright © GitHub.com/kaosborn 2017")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: ComVisible (false)]
+[assembly: Guid ("1e94ec00-73c2-4d6b-b997-0ff848c5d1ff")]
+[assembly: AssemblyVersion ("0.1.0.0")]
---
+++ Test400/TestEnumeration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kaos.SysIo;
+
+namespace Test400
+{
+    [TestClass]
+    public class TestDirVector
+    {
+        string root = "Targets";
+        string rootD1 = "Targets\\D1";
+        string[] expectedL = { "12.txt", "8A.txt" };
+        string[] expectedN = { "8A.txt", "12.txt" };
+        string[] expectedD = { "", "D1" };
+
+        [TestMethod]
+        public void TestFiles1()
+        {
+            int ix = 0;
+            foreach (string lx in DirNode.Vector.EnumerateFiles (rootD1))
+            {
+                Assert.IsTrue (ix < expectedL.Length);
+                Assert.IsTrue (lx.EndsWith (rootD1 + "\\" + expectedL[ix]));
+                ++ix;
+            }
+
+            Assert.AreEqual (expectedL.Length, ix);
+        }
+
+
+        [TestMethod]
+        public void TestFiles2()
+        {
+            int ix = 0;
+            foreach (string lx in DirNode.Vector.EnumerateFiles (rootD1, Ordering.Natural))
+            {
+                Assert.IsTrue (ix < expectedL.Length);
+                Assert.IsTrue (lx.EndsWith (rootD1 + "\\" + expectedN[ix]));
+                ++ix;
+            }
+
+            Assert.AreEqual (expectedN.Length, ix);
+        }
+
+
+        [TestMethod]
+        public void TestDirs1()
+        {
+            int ix = 0;
+            foreach (string lx in DirNode.Vector.EnumerateDirectories (root, Ordering.Lexical))
+            {
+                Assert.IsTrue (ix < expectedD.Length);
+                if (ix == 0)
+                    Assert.IsTrue (lx.EndsWith ("\\" + root));
+                else
+                    Assert.IsTrue (lx.EndsWith ("\\" + root + "\\" + expectedD[ix]));
+                ++ix;
+            }
+
+            Assert.AreEqual (expectedN.Length, ix);
+        }
+    }
+}
