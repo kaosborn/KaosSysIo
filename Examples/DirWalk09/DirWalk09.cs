9b8c39cb3fec73559cdec825bc901686c82cd6c0
kaosborn <kaosborn@users.noreply.github.com>
2017-06-05 23:24:59 -0700

    add example program

---
+++ Examples/DirWalk09/DirWalk09.cs
@@ -0,0 +1,27 @@
+﻿//
+// Program: DirWalk09.cs
+// Purpose: Demonstrate FxCore usage.
+//
+// Usage:
+// • To execute from a command prompt run:
+//     dotnet DirWalk09.dll
+
+using System;
+using Kaos.SysIo;
+
+namespace ExampleApp
+{
+    class DirWalk09
+    {
+        static void Main()
+        {
+            Console.WriteLine ("Directories:");
+            foreach (string dirName in DirNode.Vector.EnumerateDirectories ("."))
+                Console.WriteLine (dirName);
+
+            Console.WriteLine ("\nFiles:");
+            foreach (string fileName in DirNode.Vector.EnumerateFiles ("."))
+                Console.WriteLine (fileName);
+        }
+    }
+}
