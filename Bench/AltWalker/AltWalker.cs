1a98b2a8144dfd0fca29f5ddfebe54e91201287e
kaosborn <kaosborn@users.noreply.github.com>
2017-06-11 23:50:12 -0700

    add AltWalker project

---
+++ Bench/AltWalker/AltWalker.cs
@@ -0,0 +1,24 @@
+﻿//
+// Program: AltWalker.cs
+// Purpose: Demonstrate usage of the LiteDirWalker class.
+//
+
+using System;
+using System.IO;
+using AltSysIo;
+
+namespace BenchApp
+{
+    class LiteWalkBench01
+    {
+        static void Main()
+        {
+            foreach (var dx in new LiteDirWalker (".."))
+            {
+                Console.WriteLine (dx);
+                foreach (var fx in Directory.EnumerateFiles (dx))
+                    Console.WriteLine ("  " + fx);
+            }
+        }
+    }
+}
---
+++ Bench/AltWalker/LiteDirWalker.cs
@@ -0,0 +1,54 @@
+﻿//
+// Project: KaosSysIo
+// File:    LiteDirWalker.cs
+// Purpose: Define lite alternative to the DirNode class.
+//          
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace AltSysIo
+{
+    /// <summary>Provide iterator for directory traversal.</summary>
+    public class LiteDirWalker : IEnumerable<string>
+    {
+        private string[] dirs;
+        private int index;
+
+        /// <summary>Generate names of subdirecties under the specified directory.</summary>
+        /// <param name="root">The path for which subdirectory names are yielded.</param>
+        public LiteDirWalker (string root)
+        { this.dirs = new string[] { root }; }
+
+        private LiteDirWalker (string[] dirs)
+        { this.dirs = dirs; }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        { return GetEnumerator(); }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            var stack = new Stack<LiteDirWalker>();
+            for (var node = this;;)
+            {
+                string dirName = node.dirs[node.index];
+                yield return dirName;
+
+                if (Directory.Exists (dirName))
+                {
+                    string[] subdirs = Directory.GetDirectories (dirName);
+                    if (subdirs.Length > 0)
+                    {
+                        stack.Push (node);
+                        node = new LiteDirWalker (subdirs);
+                        continue;
+                    }
+                }
+
+                for (; ++node.index >= node.dirs.Length; node = stack.Pop())
+                    if (stack.Count == 0)
+                        yield break;
+            }
+        }
+    }
+}
