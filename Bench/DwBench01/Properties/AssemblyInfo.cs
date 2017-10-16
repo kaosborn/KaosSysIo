12291fe2a622ba77fc1ac2330a9957dfdbc53972
kaosborn <kaosborn@users.noreply.github.com>
2017-10-16 00:16:39 -0700

    rework bench layout

---
+++ Bench/DwBench01/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("DwBench01")]
+[assembly: AssemblyProduct ("KaosSysIo")]
+[assembly: ComVisible (false)]
+[assembly: Guid ("b7001894-8b0b-4253-9e53-fedfb2f4f7e4")]
+[assembly: AssemblyVersion ("0.1.0.0")]
