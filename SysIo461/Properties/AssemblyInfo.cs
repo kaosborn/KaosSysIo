70d56e340ca1862998b0284171331576fdd06378
kaosborn <kaosborn@users.noreply.github.com>
2017-10-16 20:03:45 -0700

    add SysIo461, Test461 projects

---
+++ SysIo461/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("SysIo461")]
+[assembly: AssemblyProduct ("KaosSysIo")]
+[assembly: AssemblyCopyright ("Copyright © github.com/kaosborn 2017")]
+[assembly: ComVisible (false)]
+[assembly: Guid ("fcf5cc8d-ffb4-48e5-a1a2-8f295833bbfb")]
+[assembly: AssemblyVersion ("0.1.0.0")]
