dc34e26bc4d44829b4af0ce9fa371ed94e8c50f2
kaosborn <kaosborn@users.noreply.github.com>
2018-09-26 21:45:25 -0700

    rev to 0.5.1.0; bump year to 2018

--- SysIo461/Properties/AssemblyInfo.cs
+++ SysIo461/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle ("SysIo461")]
 [assembly: AssemblyProduct ("KaosSysIo")]
-[assembly: AssemblyCopyright ("Copyright © github.com/kaosborn 2017")]
+[assembly: AssemblyCopyright ("Copyright © github.com/kaosborn 2017-2018")]
 [assembly: ComVisible (false)]
 [assembly: Guid ("fcf5cc8d-ffb4-48e5-a1a2-8f295833bbfb")]
 [assembly: AssemblyVersion ("0.1.0.0")]
