939a982a634d39e8ec13873b2fb85ac8db9b46ee
kaosborn <kaosborn@users.noreply.github.com>
2018-09-28 01:17:20 -0700

    rev to 0.6.0.0

--- Test461/Properties/AssemblyInfo.cs
+++ Test461/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle ("Test461")]
 [assembly: AssemblyProduct ("KaosSysIo")]
-[assembly: AssemblyCopyright ("Copyright © github.com/kaosborn 2017")]
+[assembly: AssemblyCopyright ("Copyright © github.com/kaosborn 2017-2018")]
 [assembly: ComVisible (false)]
 [assembly: Guid ("931d3fe9-bc3f-4ef0-be4b-1b9fa08dd898")]
 [assembly: AssemblyVersion ("0.1.0.0")]
